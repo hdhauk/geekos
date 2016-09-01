48047ffd0a9ad7e1029675740bcab85a3679172e
Neil Spring <nspring@cs.umd.edu>
2016-09-01 19:22:04 -0400

    updates meant to obsolete display_opt and random_port scripts that fail when checking out from git, then running vagrant from a windows host

--- provision.sh
+++ provision.sh
@@ -1,8 +1,13 @@
 # This file handles installing necessary ubuntu packages when
 # using Vagrant.
 
+# x11-utils for xdpyinfo as used in display_option detection.
+# dos2unix in case running on a windows host where git has bollocksed the line endings, making
+#  script files not executable.
+
 apt-get update
-sudo apt-get -y install build-essential nasm libc6-dev-i386 git gdb
+apt-get upgrade
+sudo apt-get -y install build-essential nasm libc6-dev-i386 git gdb x11-utils dos2unix
 sudo apt-get -y build-dep qemu
 # This is Jeff's patched qemu, maintaining fixes for accurate SMP timing.
 git clone http://git.dyninst.org/qemu.git
